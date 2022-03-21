f1ebe1db2f99d6d97363edcd42cd246b60fa08b6
sontn1309 <51114397+sontn1309@users.noreply.github.com>
2022-03-21 23:18:54 +0700

    update screenshoots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1819d4bca6e844e599883118be0193f-749990246.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
